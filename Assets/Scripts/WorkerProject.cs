89cb336c8105d545beb5f335aac4bd49b976eca0
JoeyCranberry <carberry.joey@gmail.com>
2022-10-19 11:50:12 -0700

    Added Initialize
    Useful for when objective needs to addComponent to create a project parent

--- Assets/Scripts/WorkerProject.cs
+++ Assets/Scripts/WorkerProject.cs
@@ -10,9 +10,17 @@ public class WorkerProject : MonoBehaviour
     public List<WorkerObjective> objectives = new List<WorkerObjective>();
     public ObjectivePriority priority;
     public DateTime createdDt;
+
+    public void Initialize(ObjectivePriority priority)
+    {
+        this.priority = priority;
+        createdDt = DateTime.Now;
+    }
 }
+
 public enum ObjectivePriority
 {
+    MANUAL,
     CRITICAL,
     IMPORTANT,
     NORMAL,
